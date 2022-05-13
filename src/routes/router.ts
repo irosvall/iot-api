32e53b340dee5213d794c3d12e6089d1ef49cfc7
Ida Rosvall <ir222gn@student.lnu.se>
2022-05-13 11:36:39 +0200

    Add plant router to main router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,13 +1,20 @@
-import express, { Request, Response, NextFunction } from 'express'
+/**
+ * The main router.
+ */
+
+import express, {Request, Response, NextFunction} from 'express'
 import createError from 'http-errors'
+import {router as plantRouter} from "./plant-router";
 
 export const router = express.Router()
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
-        res
-            .status(200)
-            .json({message: 'Welcome to the API!'})
-    })
+router.get('/', (req: Request, res: Response) => {
+    res
+        .status(200)
+        .json({message: 'Welcome to the API!'})
+})
+
+router.use('/plant-details', plantRouter)
 
 // Return 404 on invalid paths.
 router.use('*', (req: Request, res: Response, next: NextFunction) => next(createError(404)))
